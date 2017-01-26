63117ff1b44922e439dea8f1c28d11223ee37de6
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2017-01-26 22:31:31 +0000

    reg form dispatching

--- src/app/modules/user/registration/RegistrationForm.jsx
+++ src/app/modules/user/registration/RegistrationForm.jsx
@@ -5,55 +5,6 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import './register-user.scss';
 
-const required = value => value ? undefined : 'Required'
-const maxLength = max => value =>
-    value && value.length > max ? `Must be ${max} characters or less` : undefined;
-const minLength = min => value =>
-    value && value.length < min ? `Must be ${min} characters or more` : undefined
-
-const maxLength60 = maxLength(60);
-const minLength8 = maxLength(8);
-
-const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
-const minValue = min => value =>
-    value && value < min ? `Must be at least ${min}` : undefined
-const minValue18 = minValue(18);
-const email = value =>
-    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-        'Invalid email address' : undefined;
-const tooOld = value =>
-    value && value > 65 ? 'You might be too old for this' : undefined;
-
-
-const validate = values => {
-    const errors = {};
-    if (!values.name) {
-        errors.name = 'Required'
-    } else if (values.name.length > 60) {
-        errors.name = 'Must be 60 characters or less'
-    }
-    if (!values.email) {
-        errors.email = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-    }
-    if (!values.password) {
-        errors.password = 'Required'
-    } else if (values.password.length < 8) {
-        errors.password = 'Must be at least 8 letters and/or numbers'
-    }
-    if (!values.passwordConfirm) {
-        errors.passwordConfirm = 'Required'
-    } else if (values.passwordConfirm.length < 8) {
-        errors.passwordConfirm = 'Must be at least 8 letters and/or numbers'
-    } else if (values.password !== values.passwordConfirm) {
-        errors.passwordConfirm = 'Must match password';
-    }
-
-    return errors;
-};
-
-
 const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
     <TextField hintText={label}
                floatingLabelText={label}
@@ -63,7 +14,7 @@ const renderTextField = ({ input, label, meta: { touched, error }, ...custom }) 
     />
 );
 
-const FieldLevelValidationForm = (props) => {
+const RegistrationForm = (props) => {
     const { handleSubmit, pristine, reset, submitting } = props;
     return (
         <form onSubmit={handleSubmit} className="register-user">
@@ -89,11 +40,6 @@ const FieldLevelValidationForm = (props) => {
         </form>
     )
 };
-
-export default reduxForm({
-    form: 'fieldLevelValidation', // a unique identifier for this form
-    validate
-})(FieldLevelValidationForm)
 
 
 // const RegistrationForm = (props) => (
@@ -168,4 +114,4 @@ export default reduxForm({
 // //     form: 'Registration' // a unique name for this form
 // // })(RegistrationForm);
 //
-// export default RegistrationForm;
+export default RegistrationForm;
--- src/app/modules/user/registration/RegistrationPage.jsx
+++ src/app/modules/user/registration/RegistrationPage.jsx
@@ -1,30 +1,66 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 import RegistrationForm from './RegistrationForm';
 import {registerUser} from '../actions/user-actions';
 
-// get state we are interested in
-const mapStateToProps = (state) => {
-    return {
-        //registration: state.userRegistration
-    }
+const onSubmit = (values, dispatch) => {
+    console.log(values);
+    let userDetails = {name: values.name, password: values.password, email: values.email };
+    dispatch(registerUser(userDetails));
 };
 
-// we are interested in these dispatchers
-const mapDispatchToProps = (dispatch) => {
-    return {
-        handleSubmit: (userDetails) => {
-            dispatch(registerUser(userDetails));
-        },
-        reset: () => {
+const required = value => value ? undefined : 'Required'
+const maxLength = max => value =>
+    value && value.length > max ? `Must be ${max} characters or less` : undefined;
+const minLength = min => value =>
+    value && value.length < min ? `Must be ${min} characters or more` : undefined
 
-        }
+const maxLength60 = maxLength(60);
+const minLength8 = maxLength(8);
+
+const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
+const minValue = min => value =>
+    value && value < min ? `Must be at least ${min}` : undefined
+const minValue18 = minValue(18);
+const email = value =>
+    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+        'Invalid email address' : undefined;
+const tooOld = value =>
+    value && value > 65 ? 'You might be too old for this' : undefined;
+
+
+const validate = values => {
+    const errors = {};
+    if (!values.name) {
+        errors.name = 'Required'
+    } else if (values.name.length > 60) {
+        errors.name = 'Must be 60 characters or less'
     }
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+    if (!values.password) {
+        errors.password = 'Required'
+    } else if (values.password.length < 8) {
+        errors.password = 'Must be at least 8 letters and/or numbers'
+    }
+    if (!values.passwordConfirm) {
+        errors.passwordConfirm = 'Required'
+    } else if (values.passwordConfirm.length < 8) {
+        errors.passwordConfirm = 'Must be at least 8 letters and/or numbers'
+    } else if (values.password !== values.passwordConfirm) {
+        errors.passwordConfirm = 'Must match password';
+    }
+
+    return errors;
 };
 
-// const RegistrationForm = (
-//     <RegistrationForm handleSubmit="">
-// )
+export default reduxForm({
+    form: 'fieldLevelValidation', // a unique identifier for this form
+    validate,
+    onSubmit
+})(RegistrationForm)
 
-// connect them
-export default connect(mapStateToProps, mapDispatchToProps)(RegistrationForm);
