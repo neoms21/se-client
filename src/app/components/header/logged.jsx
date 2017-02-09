8fe983a7703c17003c8c988a5712ed1920602edc
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2017-02-09 16:01:04 +0000

    submit using redux form

---
+++ src/app/components/header/logged.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+const Logged = (props) => (
+  <IconMenu
+  {...props}
+  iconButtonElement={<IconButton>
+                         <MoreVertIcon />
+                       </IconButton>}
+  targetOrigin={{
+    horizontal: 'right',
+    vertical: 'top'
+  }}
+  anchorOrigin={{
+    horizontal: 'right',
+    vertical: 'top'
+  }}>
+    <MenuItem primaryText='Refresh' />
+    <MenuItem primaryText='Help' />
+    <MenuItem primaryText='Sign out' />
+  </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+export default Logged;
