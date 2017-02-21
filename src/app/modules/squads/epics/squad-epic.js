62d7606b1c49024c279c3136793073bae91e2179
neoms21 <neoms21@gmail.com>
2017-02-21 20:58:36 +0000

    no error now but state change doesn't happen

--- src/app/modules/squads/epics/squad-epic.js
+++ src/app/modules/squads/epics/squad-epic.js
@@ -1,15 +1,11 @@
 import * as ActionTypes from '../actions/squad.actionTypes';
 import {sendCommand} from '../../../services/server-service';
 import * as squadActions from '../actions/squad-actions';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import {connect} from 'react-redux';
 
-export const createSquadEpic = function xyz(action$) {
-    return action$.ofType(ActionTypes.CREATE_SQUAD)
-        .switchMap(action => {
-            console.log(action);
-            return sendCommand('CreateSquad', action.squad)
-            // .subscribe(ev => squadActions.createSquadSuccess(action.squad) // output success
-            //     , err => squadActions.createSquadFailure(err)); // oops failure
-        });
-};
+
+export const createSquadEpic = action$ => action$.ofType(ActionTypes.REGISTER_USER)
+    .mergeMap(action => sendCommand('CreateSquad', action.squad)
+        .map(ev => ev.isFailure ? squadActions.createSquadFailure(ev.errors)
+            : squadActions.createSquadSuccess(action.squad)));
