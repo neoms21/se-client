58c8cfaf150285b78a63045882ff14a0381ca9c7
neoms21 <neoms21@gmail.com>
2017-03-09 22:12:16 +0000

    some styling for squads list.. getting the hang of it now

--- src/app/modules/squads/views/squad.jsx
+++ src/app/modules/squads/views/squad.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import './squad-list.scss'
+import './squad-item.scss'
+import Edit from '../../../svg/edit'
 
 export default function Squad(props) {
 
-  console.log(props);
-  return  <div className="squad-container">
+    console.log(props);
+    return <div className="squad-container">
 
-    <span>
+    <span className="squad-container__name">
       {props.name}
     </span>
 
-    <svg
-      className="squad-container__delete"
-      viewBox="0 0 24 24" onClick={props.onClick}>
+        <Edit color="blue" onClick={props.onClick}></Edit>
 
-      <path
-        fill="#000000"
-        d="M20.71,4.04C21.1,3.65 21.1,3 20.71,2.63L18.37,0.29C18,-0.1 17.35,-0.1 16.96,0.29L15,2.25L18.75,6M17.75,7L14,3.25L4,13.25V17H7.75L17.75,7Z" />
 
-    </svg>
-
-  </div>;
+    </div>;
 };
--- src/app/modules/squads/views/squads.jsx
+++ src/app/modules/squads/views/squads.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import Squad from './squad'
+import './squad-list.scss'
+
+import RaisedButton from 'material-ui/RaisedButton';
 import * as squadActions from '../actions/squad-actions'
 
 class SquadsComponent extends React.Component {
@@ -15,14 +18,17 @@ class SquadsComponent extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="squad-list">
+
+                {/*<input type="button"  value="Add Squad" />*/}
+                <RaisedButton className="squad-list__button"
+                              label="Add Squad" primary={true} />
                 {this.props.squads.map(function(squad, index){
                     return <Squad key={index} name={squad.name} onClick= {()=>{
                         console.log(squad);
                       }}></Squad>;
                 })}
 
-                <input type="button" className="button" value="Add Squad" />
             </div>
         );
     }
