570a987ddad04b5129a54526cad14e32f8f90d2a
Manoj Sethi <manoj.sethi@atomus.com>
2017-03-29 18:57:32 +0100

    immediate validation works but not convincing

--- src/app/modules/squads/views/squadForm.jsx
+++ src/app/modules/squads/views/squadForm.jsx
@@ -2,7 +2,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react'
 import {FormsyText} from 'formsy-material-ui';
 import Formsy from 'formsy-react';
-
+import {TextField} from 'material-ui'
 let {Component, PropTypes} = React;
 
 
@@ -26,8 +26,14 @@ export default class CreateSquadForm extends Component {
     };
 
     handleChange = (e) => {
+        console.log(e.target.value);
         // remove error
-        this.setState({pristine: false, showError: false});
+        //this.setState({pristine: false, showError: false});
+        this.setState({
+            pristine: false,
+            squadName: e.target.value,
+            squadNameError: e.target.value.trim().length === 0 ? 'Squad name is required' : ''
+        });
     };
 
     reset = () => {
@@ -38,23 +44,19 @@ export default class CreateSquadForm extends Component {
         const {handleSubmit} = this.props;
 
         return (
-            <Formsy.Form onSubmit={handleSubmit}>
-                <FormsyText
-                    name="squadName"
-                    value={this.props.squadName}
-                    validations="minLength:1"
-                    validationError={this.errorMessages.squadNameError}
-                    required updateImmediately fullWidth={true}
-                    hintText="Enter the name of the squad"
-                    floatingLabelText="Squad name"
-                    onChange={this.handleChange}
-                />
 
+            <form onSubmit={handleSubmit}>
+                <TextField ref="position" hintText="Enter the name of the squad" className="position"
+                           floatingLabelText="Squad name" errorText={this.state.squadNameError}
+                           onChange={this.handleChange}
+                />
+                <span>{this.state.pristine} - adas</span>
                 <div className="button-row">
                     <RaisedButton label="Register" primary={true} type="submit" disabled={this.state.isSubmitting}/>
                     <RaisedButton label="Clear Values" disabled={this.state.pristine} onClick={this.reset}/>
                 </div>
-            </Formsy.Form>
+            </form>
+
         );
     };
 }
