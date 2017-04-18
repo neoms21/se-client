f840a3c9c75b2cad01b19dd04d7a14fdc573b95e
Manoj Sethi <msethi@qubittech.co.uk>
2017-04-18 22:43:33 +0100

    player form select

--- src/app/modules/players/views/playerForm.jsx
+++ src/app/modules/players/views/playerForm.jsx
@@ -4,13 +4,15 @@ import {Field, reduxForm} from 'redux-form'
 import TextField from 'material-ui/TextField'
 import '../../../core.scss'
 
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
 import {validateRequiredFields} from '../../../validations'
 // import asyncValidate from './asyncValidate'
 
 const validate = values => {
     console.log(values);
     let errors = {};
-    let requiredErrors = validateRequiredFields(values, ['playerName', 'age']) // find our how to combine with other validations
+    let requiredErrors = validateRequiredFields(values, ['playerName', 'age']); // find our how to combine with other validations
     let ageError = {};
     if (values.age && values.age < 8) {
         ageError.age = "Player's age must be greater than 8";
@@ -30,6 +32,14 @@ const renderTextField = ({input, label, meta: {touched, error}, ...custom}) => (
     />
 );
 
+const renderSelectField = props => (
+    <SelectField
+        floatingLabelText={props.label}
+        errorText={props.touched && props.error}
+        {...props}>
+    </SelectField>
+)
+
 const playerForm = props => {
     const {handleSubmit, pristine, reset, submitting} = props;
 
@@ -46,6 +56,13 @@ const playerForm = props => {
             </div>
             <div>
                 <Field name="phone" component={renderTextField} label="Phone"/>
+            </div>
+            <div>
+                <Field name="position" component={renderSelectField} label="Position">
+                    <MenuItem value={'striker'} primaryText="Striker"/>
+                    <MenuItem value={'goalKeeper'} primaryText="Goalkeeper"/>
+                    <MenuItem value={'midFielder'} primaryText="MidFielder"/>
+                </Field>
             </div>
             <div className="button-row">
                 <RaisedButton label="Add" primary={true} type="submit" disabled={submitting}/>
