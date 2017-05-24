6c0ee0558d3137ab3b7a47f70f5a9d60784b7de2
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2017-05-24 23:00:31 +0100

    matches

--- src/app/modules/matches/pages/edit-match/edit-match-page.jsx
+++ src/app/modules/matches/pages/edit-match/edit-match-page.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import EditMatchForm from './edit-match-form';
-import {registerMatch} from '../../actions/match-actions';
-import {connect} from 'react-redux';
-import {ServerService} from '../../../../services/server-service';
-import {sendQuery} from '../../../../services/server-service';
-import {reduxForm} from 'redux-form';
+import { registerMatch } from '../../actions/match-actions';
+import { connect } from 'react-redux';
+import { ServerService } from '../../../../services/server-service';
+import { sendQuery } from '../../../../services/server-service';
+import { reduxForm } from 'redux-form';
 
 function mapStateToProps(state, ownProps) {
-    return {
-        errors: state.matches.errors,
-        squads: state.squads.squads,
-        disabled: false
-    };
+  return {
+    errors: state.matches.errors,
+    squads: state.squads.squads,
+    disabled: false
+  };
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        handleSubmit: (values) => {
-            dispatch(registerMatch(values));
-        }
+  return {
+    handleSubmit: (values) => {
+      dispatch(registerMatch(values));
     }
+  }
 };
 
 // do a redux subscription
