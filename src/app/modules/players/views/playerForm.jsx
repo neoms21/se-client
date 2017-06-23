d3a326e368fec3529b834c5914f070795d5a28a9
Manoj Sethi <Manoj.Sethi@metrobank.plc.uk>
2017-06-23 22:23:01 +0100

    material ui select for player position

--- src/app/modules/players/views/playerForm.jsx
+++ src/app/modules/players/views/playerForm.jsx
@@ -4,6 +4,24 @@ import {Field, reduxForm} from 'redux-form'
 import textField from '../../../elements/textField'
 import '../../../core.scss'
 import {stopSubmit} from 'redux-form';
+import {SelectField, MenuItem} from 'material-ui'
+
+const items = [
+    <MenuItem key={1} value={1} primaryText="Never"/>,
+    <MenuItem key={2} value={2} primaryText="Every Night"/>,
+    <MenuItem key={3} value={3} primaryText="Weeknights"/>,
+    <MenuItem key={4} value={4} primaryText="Weekends"/>,
+    <MenuItem key={5} value={5} primaryText="Weekly"/>,
+];
+
+const renderSelectField = ({ input, label, meta: { touched, error }, children }) => (
+    <SelectField
+        floatingLabelText={label}
+        errorText={touched && error}
+        {...input}
+        onChange={(event, index, value) => input.onChange(value)}
+        children={children}/>
+)
 
 class PlayerForm extends React.Component {
 
@@ -11,6 +29,10 @@ class PlayerForm extends React.Component {
     mySubmit(values) {
         console.log(this.props.match.params.id);
         return this.props.onSave(values, this.props.match.params.id);
+    }
+
+    componentWillUnmount() {
+        this.props.onUnmount();
     }
 
     //
@@ -36,6 +58,14 @@ class PlayerForm extends React.Component {
                 </div>
                 <div>
                     <Field name="phone" component={textField} label="Phone"/>
+                </div>
+                <div>
+
+                    <Field name="position" component={renderSelectField} label="Position">
+                        <MenuItem value={'ff0000'} primaryText="Red"/>
+                        <MenuItem value={'00ff00'} primaryText="Green"/>
+                        <MenuItem value={'0000ff'} primaryText="Blue"/>
+                    </Field>
                 </div>
                 <div className="button-row">
                     <RaisedButton label="Add" primary={true} type="submit" disabled={submitting}/>
